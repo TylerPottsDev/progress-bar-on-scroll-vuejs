7529a68fa66be188e502f21253cd4908c096dd97
Stuart Kirkland <stuart.kirkland@zincdigital.com>
2020-01-08 23:19:26 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <header>
+        <h1>Scroll Progress Bar</h1>
+      </header>
+      <Progressbar :progress="progress" />
+      <Post @progressUpdate="setProgress" />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from './components/Post';
+import Progressbar from './components/Progressbar';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Post,
+    Progressbar
+  },
+  data () {
+    return {
+      progress: 0
+    }
+  },
+  methods: {
+    setProgress (progress) {
+      this.progress = progress;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    font-family: 'montseratt', sans-serif;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    overflow: hidden;
+  }
+
+  header {
+    background-color: #313131;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  header h1 {
+    color: #FFF;
+    font-size: 28px;
+    text-align: center;
+  }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,101 @@
+<template>
+    <article ref="el">
+        <div class="content">
+            <h2>The Article title</h2>
+            <h3>An Article subtitle slightly longer than the title</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus veritatis neque nisi, temporibus itaque libero inventore rerum excepturi ratione, ipsam delectus harum suscipit voluptate, praesentium amet numquam! Natus dicta corrupti culpa veritatis eius dolor tempora error accusantium perferendis quo voluptatum eligendi est iure repudiandae explicabo iste, beatae cupiditate magni a?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsam quam impedit sapiente vel iusto dicta voluptate, dignissimos dolores officia ullam, perspiciatis autem, sequi quasi ex quos ipsum? Sequi iste voluptate accusamus porro deserunt veritatis. Odio repudiandae sint sit hic, error delectus. Laudantium sed perspiciatis, architecto at rerum, temporibus iusto ex modi recusandae iste doloremque deserunt facere. Ad repudiandae veniam illo et ratione repellat culpa porro qui, amet quos labore ullam officiis eveniet quia quo debitis quaerat, repellendus inventore expedita cum laborum sequi commodi eos. Esse delectus, necessitatibus debitis facere cum, nisi incidunt architecto deserunt placeat error earum dicta numquam.
+            </p>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'post',
+    mounted () {
+        this.$el.addEventListener('scroll', function() {
+            let scrollTop = this.$el.scrollTop;
+            let p_height = this.$el.clientHeight;
+            let c_height = this.$el.scrollHeight;
+            
+            let scrollPercent = (scrollTop) / (c_height - p_height);
+            let scrollPercentRounded = Math.round(scrollPercent*100);
+            
+            this.$emit('progressUpdate', scrollPercentRounded);
+        }.bind(this));
+    }
+}
+</script>
+
+<style>
+    article {
+        width: 100%;
+        flex: 1 1 100%;
+        max-height: calc(100vh - 80px);
+        background-color: #FFF;
+        overflow: scroll;
+    }
+
+    article .content {
+        padding: 25px;
+    }
+
+    article .content h2 {
+        color: #11224F;
+        font-size: 32px;
+        font-weight: 900;
+        margin-bottom: 10px;
+    }
+
+    article .content h3 {
+        color: #676767;
+        font-size: 22px;
+        font-weight: 300;
+        margin-bottom: 30px;
+    }
+
+    article .content p {
+        color: #313131;
+        font-size: 18px;
+        line-height: 1.5;
+        font-weight: 300;
+        margin-bottom: 15px;
+    }
+</style>
